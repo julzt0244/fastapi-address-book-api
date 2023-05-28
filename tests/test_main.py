270f7e2b3588a145b40d1c5385c5b760494eb7a3
jultz0244 <julian.tanz@yahoo.com>
2023-05-28 21:23:36 +1000

    rename test_main functions

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,6 @@ from address_book.main import app
 test_client = TestClient(app)
 
 
-def test_root():
+def test_can_browse_root_page():
     response = test_client.get("/")
     assert response.status_code == 200
